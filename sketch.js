3406395c22686f5a7471ecb7ffa7a9407e9d4fe2
kolamax25 <75153987+kolamax25@users.noreply.github.com>
2021-02-02 16:59:04 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var database;
+var read;
+var position
+var ball;
+
+function setup(){
+    database = firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    read = database.ref('ball/position');
+    read.on("value", readPosition, showError)
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+10);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    
+    database.ref('ball/position').set({
+
+        'x': position.x + x,
+        'y': position.y + y
+
+    })
+
+
+
+}
+
+function readPosition(data){
+
+    position = data.val();
+    ball.x = position.x
+    ball.y = position.y
+}
+
+function showError(){
+
+    console.log("Error")
+}
